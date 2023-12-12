8fbfa9aa8fc2cc51f5377746c7dba7babb864a1a
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-12-12 00:08:00 -0500

    oh damn

--- client/main.js
+++ client/main.js
@@ -315,6 +315,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (request.action == "getSource") {
           // this.pageSource = request.source;
           const html = request.source;
+          const xs = html.split("->->->->->:-!");
 
           //Update Iters
           iters += 1;
@@ -323,83 +324,60 @@ document.addEventListener("DOMContentLoaded", async () => {
           document.getElementById(
             "minsta-iters"
           ).innerText = `Iters : ${iters}`;
-          document.getElementById("minsta-content").innerText = html;
+          document.getElementById("minsta-content").innerHTML = xs[1];
 
           //Save to localStorage
+          // document.getElementById("minsta").innerText = ("Monitoring Chat with " + xs[0] + " .....");
           localStorage.setItem("minsta", html);
         }
       });
 
       function injection() {
-        console.log("ran");
         //Get Title of Conversation
-        const convoTitle = document.querySelector(".x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft").innerText;
-        console.log(convoTitle);
+        const convoThings = document.querySelectorAll(
+          ".x1lliihq.x193iq5w.x6ikm8r.x10wlt62.xlyipyv.xuxw1ft"
+        )
+        if(convoThings.length == 0) return;
+
+        const convoTitle = convoThings[convoThings.length - 1].innerText;
 
         //Get All Messages
-        const msgs = document.querySelector(".x1n2onr6").children;
-        const actMsgs = [];
-        let lastName = "";
-        for (let i = 0; i < msgs.length; i++) {
-          //Get Actual Message
-          const msg = msgs[i].children[1].firstChild.firstChild;
+        const messages = document.querySelectorAll(
+          ".x6prxxf.x1fc57z9.x1yc453h.x126k92a"
+        );
 
+        const actMsgs = [];
+        let op = false;
+        for (let i = 0; i < messages.length; i++) {
           try {
-            //Get Name
-            const par = msgs[i].parentNode;
-            const supPar = par.parentNode;
-            const container = supPar.firstChild;
-            const nameElement = container.firstChild;
-            let name = nameElement.innerText || msg.innerText;
+            //Get Text
+            const txt = messages[i].innerText;
 
             //Check if it is our message
-            const youContainer = supPar.parentNode;
-            const spans = youContainer.childNodes;
             let isYou = false;
-            for (let i = 0; i < spans.length; i++) {
-              const element = spans[i];
-              // Check if it is a span
-              if (element.nodeName === "SPAN") {
-                const label = element.ariaLabel;
-                // console.log(label);
-                if (label === null || undefined) {
-                  //Nothing. Standard Whatsapp
-                  // console.log("Standard Whatsapp Detected");
-                  continue;
-                } else {
-                  //Sometimes the label might contain the convo name, for direct messages
-                  if (label.includes(convoTitle)) {
-                    isYou = false;
-                    continue;
-                  } else {
-                    // ABORT ABORT
-                    // console.log("MSG by User DETECTED");
-                    isYou = true;
-                    break;
-                  }
-                }
-              }
+            if(messages[i].classList.contains("xzsf02u")){
+              isYou = false;
+            }
+            else{
+              isYou = true;
             }
 
-            if (isYou === true) {
-              console.log("Message by User detected");
-              // Abort!
-            } else if (isYou === false) {
-              //If the name and sender are equal, take the last valid name (WhatsApp :L)
-              if (name === msg.innerText) {
-                name = lastName;
-              } else if (name !== msg.innerText) {
-                lastName = name;
+            // If we have something open and we're the user, close
+            if(isYou === true && actMsgs.length === 0){
+              // sorry
+            }
+            else{
+              if(isYou === true){
+                actMsgs[actMsgs.length - 1]["messages_us"].push(txt)
+                op = false;
               }
-
-              //If the name is blank, we're in a direct chat, so the name will be the title
-              if (name === "") {
-                name = convoTitle;
-                lastName = convoTitle;
+              if(isYou === false && op === true){
+                actMsgs[actMsgs.length - 1]["messages_other"].push(txt)
               }
-
-              //Assign each message a unique id according to the message and name
-              actMsgs.push({ sender: name, msg: msg.innerText });
+              if(isYou === false && op === false){
+                actMsgs.push({messages_other : [txt], messages_us : []});
+                op = true;
+              }
             }
           } catch (err) {
             // Our Message, so don't do anything
@@ -407,70 +385,17 @@ document.addEventListener("DOMContentLoaded", async () => {
           }
         }
 
-        //Format
-        const output = {
-          title: convoTitle,
-          msgs: actMsgs,
-        };
-
-        //Write to localStorage
-        const storage = JSON.parse(
-          localStorage.getItem("smoothtalker_buf") || "[]"
-        );
+        // Persistance because.... WHY NOT?
+        const act = JSON.parse(localStorage.getItem("smoothtalker_buf_insta" || "[]")) || [];
+        const new_act = [... new Set(act.concat(actMsgs))];
 
         //Create LS Output
-        let ls_output = [];
-        let found = false;
-        for (let i = 0; i < storage.length; i++) {
-          let chat = storage[i];
-          let nChat = {};
-          let c_msgs = chat.msgs;
-
-          nChat.title = chat.title;
-          if (chat.title === output.title) {
-            console.log(`Updated Smoothtalker Data for ${convoTitle}`);
-            //Append
-            c_msgs = c_msgs.concat(output.msgs);
-
-            //Remove duplicates
-            let unique_msgs = [];
-            for (let i = 0; i < c_msgs.length; i++) {
-              const msg = c_msgs[i];
-              let is_unique = true;
-              for (let j = 0; j < unique_msgs.length; j++) {
-                const unique_msg = unique_msgs[j];
-                if (
-                  msg.sender === unique_msg.sender &&
-                  msg.msg === unique_msg.msg
-                ) {
-                  is_unique = false;
-                }
-              }
-
-              if (is_unique === true) {
-                unique_msgs.push(msg);
-              }
-            }
-            c_msgs = unique_msgs;
-
-            //Set found to true
-            found = true;
-          }
-          nChat.msgs = c_msgs;
-          ls_output.push(nChat);
-        }
-
-        //If our chat isn't a part, add it
-        if (found === false) {
-          ls_output.push(output);
-        }
-
-        localStorage.setItem("smoothtalker_buf", JSON.stringify(ls_output));
+        localStorage.setItem("smoothtalker_buf_insta", JSON.stringify(new_act));
 
         chrome.runtime.sendMessage({
           action: "getSource",
-          source: JSON.stringify({
-            ls_output,
+          source: convoTitle + "->->->->->:-!" + JSON.stringify({
+            new_act
           }),
         });
 
