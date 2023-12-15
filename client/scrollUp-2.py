f7ee840c8e256adc2405724d0daac457a63ba3bd
Lakshya Jain <minecraftpogtechno@gmail.com>
2023-12-15 16:59:13 -0500

    call-me-up

--- client/scrollUp-2.py
+++ client/scrollUp-2.py
@@ -2,11 +2,11 @@ from pynput.mouse import Controller
 from time import sleep
 
 mouse = Controller()
-dur = 0.1 * 60
+dur = 0.05 * 60
 offset = 0.15 * 60
 
 sleep(offset)
 while True:
-    mouse.scroll(0, 10)
+    mouse.scroll(0, 20)
     print("d")
     sleep(dur)
